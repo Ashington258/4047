6304a1431b4f44d0cb8da22eb68dcfca659a8a68
xiaoyuan <2733740879@qq.com>
2024-08-01 03:47:41 +0800

    编码器模块，pwm模块

--- software/MSPM0G3507_TB6612/Hardware/motor.c
+++ software/MSPM0G3507_TB6612/Hardware/motor.c
@@ -67,25 +67,25 @@ void Set_PWM(int pwma,int pwmb)
 	{
 		DL_GPIO_setPins(AIN1_PORT,AIN1_PIN_12_PIN);
 		DL_GPIO_clearPins(AIN2_PORT,AIN2_PIN_13_PIN);
-		DL_Timer_setCaptureCompareValue(PWM_0_INST,ABS(pwma),GPIO_PWM_0_C0_IDX);
+		DL_TimerA_setCaptureCompareValue(PWM_1_INST, ABS(pwma), GPIO_PWM_1_C0_IDX);
 	}
 	else
 	{
-		DL_GPIO_setPins(AIN2_PORT,AIN2_PIN_13_PIN);
-		DL_GPIO_clearPins(AIN1_PORT,AIN1_PIN_12_PIN);
-		DL_Timer_setCaptureCompareValue(PWM_0_INST,ABS(pwma),GPIO_PWM_0_C0_IDX);
+		DL_GPIO_clearPins(AIN2_PORT,AIN2_PIN_13_PIN);
+		DL_GPIO_setPins(AIN1_PORT,AIN1_PIN_12_PIN);
+		DL_TimerA_setCaptureCompareValue(PWM_1_INST, ABS(pwma), GPIO_PWM_1_C0_IDX);
 	}
 	if(pwmb>0)
 	{
 		DL_GPIO_setPins(BIN1_PORT,BIN1_Pin_Bin1_PIN);
 		DL_GPIO_clearPins(BIN2_PORT,BIN2_Pin_Bin2_PIN);
-		DL_Timer_setCaptureCompareValue(PWM_0_INST,ABS(pwmb),GPIO_PWM_0_C1_IDX);
+		DL_TimerA_setCaptureCompareValue(PWM_1_INST, ABS(pwmb), GPIO_PWM_1_C1_IDX);
 	}
     else
 	{
 		DL_GPIO_setPins(BIN2_PORT,BIN2_Pin_Bin2_PIN);
 		DL_GPIO_clearPins(BIN1_PORT,BIN1_Pin_Bin1_PIN);
-		DL_Timer_setCaptureCompareValue(PWM_0_INST,ABS(pwmb),GPIO_PWM_0_C1_IDX);
+		DL_TimerA_setCaptureCompareValue(PWM_1_INST, ABS(pwmb), GPIO_PWM_1_C1_IDX);
 	}		
 
 }
